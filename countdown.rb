0da5f8f5905f12f27f28b3e1e290fa69535b09da
mccray austin <tubach75@hotmail.com>
2020-09-18 13:13:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown(countdown_output)
   return "HAPPY NEW YEAR!"
 end
 countdown(10)
+
+
+def countdown_with_sleep(countdown_output)
+  while countdown_output > 0
+    puts "#{countdown_output} SECOND(S)!"
+    sleep(1)
+    countdown_output -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+countdown_with_sleep(10)
