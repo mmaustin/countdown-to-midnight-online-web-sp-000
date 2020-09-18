5ae7cdf97e9c0699736ea99b7ccc9e87ebcec8a5
mccray austin <tubach75@hotmail.com>
2020-09-18 13:08:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(countdown_output)
+  while countdown_output > 0
+    puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+countdown(10)
